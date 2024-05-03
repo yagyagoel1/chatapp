311ac51c098605f285efcc71f17a3fe6f55dfaf3
yagyagoel1 <yagyagoel87@gmail.com>
2024-05-04 04:09:35 +0530

    added tthe messages to receive on frontend

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,16 +2,19 @@ import { useState } from "react";
 import { useSocket } from "../context/SocketProvider"
 import classes from "./page.module.css"
 export default function Page(){
-  const {sendMessage} = useSocket();
+  const {sendMessage,messages} = useSocket();
   const [message,setMessage]= useState("")
   return (
     <div>
-      <div>
-        <h1>All messages will appear here</h1>
-      </div>
+     
       <div>
         <input onChange={e=>setMessage(e.target.value)} className={classes["chat-input"]} placeholder="Message..."/>
         <button onClick={e=>sendMessage(message)} className={classes["button"]}>Send</button>
+      </div>
+      <div>
+        <h1>All messages will appear here</h1>
+        {messages.map((e)=>
+          <li>{e}</li>)}
       </div>
     </div>
   )
--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -7,6 +7,7 @@ interface SocketProviderProps{
 }
 interface ISocketContext{
     sendMessage:(msg:string)=>any
+    messages:string[]
 }
 const SocketContext = React.createContext<ISocketContext|null>(null)
 
@@ -22,21 +23,29 @@ export const useSocket=()=>{
 
 export const SocketProvider :React.FC<SocketProviderProps>=({children})=>{
     const [socket,setSocket] = useState<Socket>()
+    const [messages,setMessages]=useState<string[]>([])
     const sendMessage:ISocketContext["sendMessage"] =useCallback((msg)=>{
         if(socket){
             socket.emit("event:message",{message:msg})
         }
         console.log(msg)
     },[] )
+    const onMessageRec= useCallback((msg:string)=>{
+        console.log("from server msg rec",msg)
+        const{ message}=  JSON.parse(msg)as {message:string}
+        setMessages((prev)=>[...prev,message])
+    },[]) 
     useEffect(()=>{ 
         const _socket= io("http://localhost:8000")
+        _socket.on("message",onMessageRec)
         return ()=>{
             _socket.disconnect();
+            _socket.off("message",onMessageRec)
             setSocket(undefined)
         }
     },[])
     return(
-        <SocketContext.Provider  value={null}>
+        <SocketContext.Provider  value={{sendMessage,messages}}>
             {children}
         </SocketContext.Provider>
     )
