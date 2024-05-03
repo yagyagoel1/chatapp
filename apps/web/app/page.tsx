9bba3584f2d414fd4b9d5fb31cefcc3736c0ab79
yagyagoel1 <yagyagoel87@gmail.com>
2024-05-04 03:16:58 +0530

    build the frontend bits of the project

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
+import { useSocket } from "../context/SocketProvider"
 import classes from "./page.module.css"
 export default function Page(){
+  const {sendMessage} = useSocket();
+  const [message,setMessage]= useState("")
   return (
     <div>
       <div>
         <h1>All messages will appear here</h1>
       </div>
       <div>
-        <input className={classes["chat-input"]} placeholder="Message..."/>
-        <button className={classes["button"]}>Send</button>
+        <input onChange={e=>setMessage(e.target.value)} className={classes["chat-input"]} placeholder="Message..."/>
+        <button onClick={e=>sendMessage(message)} className={classes["button"]}>Send</button>
       </div>
     </div>
   )
--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import React, { useCallback, useContext, useEffect } from "react"
-import { io } from "socket.io-client"
+import React, { useCallback, useContext, useEffect, useState } from "react"
+import { Socket, io } from "socket.io-client"
 interface SocketProviderProps{
     children : React.ReactNode
 }
@@ -21,13 +21,18 @@ export const useSocket=()=>{
 
 
 export const SocketProvider :React.FC<SocketProviderProps>=({children})=>{
+    const [socket,setSocket] = useState<Socket>()
     const sendMessage:ISocketContext["sendMessage"] =useCallback((msg)=>{
+        if(socket){
+            socket.emit("event:message",{message:msg})
+        }
         console.log(msg)
     },[] )
     useEffect(()=>{ 
         const _socket= io("http://localhost:8000")
         return ()=>{
             _socket.disconnect();
+            setSocket(undefined)
         }
     },[])
     return(
