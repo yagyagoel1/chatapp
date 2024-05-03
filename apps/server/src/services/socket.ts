b5ec2baecaceb872ed9d1fed21cbec4135f98a55
yagyagoel1 <yagyagoel87@gmail.com>
2024-05-04 03:39:47 +0530

    added redis pubsub to the backend

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,5 +1,18 @@
 import { Server } from "socket.io";
+import Redis from "ioredis";
 
+const pub = new Redis({
+  host: process.env.REDISHOST,
+  port: 19717,
+  username: process.env.REDISUSERNAME,
+  password: process.env.REDISPASSWORD,
+});
+const sub = new Redis({
+  host: process.env.REDISHOST,
+  port: 19717,
+  username: process.env.REDISUSERNAME,
+  password: process.env.REDISPASSWORD,
+});
 class SocketService {
   private _io: Server;
   constructor() {
@@ -10,6 +23,7 @@ class SocketService {
         origin: "*",
       },
     });
+    sub.subscribe("MESSAGES");
   }
 
   get io() {
@@ -22,7 +36,13 @@ class SocketService {
       console.log("new socket connected, ", socket.id);
       socket.on("event:message", async ({ message }: { message: string }) => {
         console.log("new message received", message);
+        await pub.publish("MESSAGES", JSON.stringify({ message }));
       });
+    });
+    sub.on("message", (channel, message) => {
+      if (channel === "MESSAGES") {
+        io.emit("message", message);
+      }
     });
   }
 }
