2b2668b3420308ba3d8b69f46a9e84375eab5cc7
yagyagoel1 <yagyagoel87@gmail.com>
2024-05-04 03:16:33 +0530

    build the frontend bits of the project

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useCallback, useEffect } from "react"
+import React, { useCallback, useContext, useEffect } from "react"
 import { io } from "socket.io-client"
 interface SocketProviderProps{
     children : React.ReactNode
@@ -9,6 +9,16 @@ interface ISocketContext{
     sendMessage:(msg:string)=>any
 }
 const SocketContext = React.createContext<ISocketContext|null>(null)
+
+export const useSocket=()=>{
+    const state =useContext(SocketContext)
+    if(!state)
+    throw new Error("state is undefined")
+    return state
+}
+
+
+
 
 export const SocketProvider :React.FC<SocketProviderProps>=({children})=>{
     const sendMessage:ISocketContext["sendMessage"] =useCallback((msg)=>{
